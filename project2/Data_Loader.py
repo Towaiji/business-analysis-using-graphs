6f6e8ffe9d40efd6bd8cd862f76b0cb8c986f93d
TanayLanghe <tanaylanghe@gmail.com>
2024-04-01 16:33:54 -0400

    Added dataload function

--- project2/Data_Loader.py
+++ project2/Data_Loader.py
@@ -1,1 +1,32 @@
+"""
+This document is used to load the data from the file into readable and usable data
+"""
+import json
 
+
+def parse(path):
+    """
+    Command used to recieve file and output its data
+    """
+    data = []
+    with open(path) as file:
+        for l in file:
+            dic = json.loads(l)
+            req_keys = ['longitude', 'latitude', 'category', 'avg_rating', 'num_of_reviews', 'hours']
+            if 'state' in dic.keys() and all(key in dic.keys() for key in req_keys):
+                if dic['state'] != "Permanently closed" and all(dic[key] is not None for key in req_keys):
+                    dic.pop('gmap_id')
+                    dic.pop('MISC')
+                    dic.pop('relative_results')
+                    dic.pop('url')
+                    dic.pop('description')
+                    dic.pop('price')
+                    data.append(dic)
+    return data[0]
+
+
+def filter(file):
+    """
+
+    :return:
+    """
