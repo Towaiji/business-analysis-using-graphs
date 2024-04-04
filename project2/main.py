32debfce4eb7de9e77c538f39a52e3987312272a
Towaiji <aliyasiraltowaiji@gmail.com>
2024-04-04 14:11:47 -0400

    Update main.py

--- project2/main.py
+++ project2/main.py
@@ -5,6 +5,7 @@ from Data_Loader import get_criteria, parse, get_states
 from visualization import visualize_businesses_on_map
 from Graph_Opertations import Graph
 
+
 def main():
     """
     main function responsible for running all files
@@ -18,10 +19,15 @@ def main():
     # Step 2: Get user-defined criteria
     min_rating, min_reviews, category = get_criteria()
 
+<<<<<<< Updated upstream
     g.filter_data(category, min_reviews, min_rating)
 
     # Step 3: Find similar businesses
     g.build_edges_based_on_criteria(category)
+=======
+    # Step 3: Find similar businesses based on criteria
+    g.build_edges_based_on_criteria(category, min_rating, min_reviews)
+>>>>>>> Stashed changes
 
     # Step 4: Visualize the similar businesses
     if g.get_vertices_data():
